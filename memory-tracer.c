f7558003726523d1cdc429a93358e62319abb305
Jonathan Lifflander <jliffla@sandia.gov>
2023-09-20 15:47:49 -0700

    Fix some bugs when allocation is required during initialization

--- memory-tracer.c
+++ memory-tracer.c
@@ -3,10 +3,20 @@
 #include <stdio.h>
 #include <dlfcn.h>
 
+static struct temp_memory {
+  size_t pos;
+  size_t allocs;
+  char buf[16384];
+} temp = { 0, 0 };
+
 static void* (*sys_malloc)(size_t) = 0;
 static void (*sys_free)(void*) = 0;
 
+static int in_initialize = 0;
+
 static void initialize_memory_tracer() {
+  in_initialize = 1;
+
   sys_malloc = dlsym(RTLD_NEXT, "malloc");
   if (!sys_malloc) {
     fprintf(stderr, "Error in `dlsym`: %s\n", dlerror());
@@ -17,12 +27,30 @@ static void initialize_memory_tracer() {
     fprintf(stderr, "Error in `dlsym`: %s\n", dlerror());
   }
 
+  in_initialize = 0;
 }
 
 void* malloc(size_t size) {
   // init on demand
   if (!sys_malloc) {
-    initialize_memory_tracer();
+    if (in_initialize) {
+
+      // We need some allocation while we are initializing
+      if (temp.pos + size < sizeof(temp.buf)) {
+        void* ptr = temp.buf + temp.pos;
+        temp.pos += size;
+        temp.allocs++;
+        return ptr;
+      } else {
+        // Not enough memory
+        fprintf(
+          stderr,
+          "Ran out of temp memory: pos=%zu, allocs=%zu\n", temp.pos, temp.allocs
+        );
+      }
+    } else {
+      initialize_memory_tracer();
+    }
   }
 
   void* ptr = sys_malloc(size);
@@ -31,9 +59,9 @@ void* malloc(size_t size) {
 }
 
 void free(void* ptr) {
-  // init on demand
-  if (!sys_free) {
-    initialize_memory_tracer();
+  // Handle a temp free
+  if (ptr >= (void*)temp.buf && ptr <= (void*)(temp.buf + temp.pos)) {
+    // We are freeing memory, but won't actually change anything
   }
 
   sys_free(ptr);
